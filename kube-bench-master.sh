c7b38acf27c929d17bb8c4f19353553dec540551
adnanghazzaal <adnanghazzaal@gmail.com>
2022-11-20 03:42:32 +0500

    asd

--- kube-bench-master.sh
+++ kube-bench-master.sh
@@ -3,7 +3,7 @@
 target_score=$(/kubebench/kube-bench master --config-dir `pwd`/cfg --config `pwd`/cfg/config.yaml --json | jq .Totals.total_fail)
 
 
-if [[ $target_score -gt 10 ]];
+if [[ $target_score -gt 15 ]];
 then
    echo 'Failure is high please remedy the fails'
    exit 1;
